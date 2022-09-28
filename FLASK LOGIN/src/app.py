9c3510590ca300caf329f0e5faf311d7a5546b2b
Matthew2720 <mhernandezrpo22@gmail.com>
2022-09-28 16:06:43 -0500

    SUBIDA DEL PROYECTO

---
+++ FLASK LOGIN/src/app.py
@@ -0,0 +1,61 @@
+from flask import Flask,render_template,url_for,request,redirect,flash
+from config import *
+from flask_login import LoginManager,login_user,logout_user,login_required
+import pyodbc
+
+#Modelos y entidades
+
+from models.entities.User import User
+from models.ModelUser import ModelUser
+
+app = Flask(__name__)
+db = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=' + direccion_servidor+';DATABASE='+nombre_bd+';UID='+nombre_usuario+';PWD=' + password)
+login_manager_app = LoginManager(app)
+
+@login_manager_app.user_loader
+def load_user(id):
+    return ModelUser.get_by_id(db,id)
+
+@app.route('/')
+def index():
+    return redirect(url_for('login'))
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    if request.method == 'POST':
+        user = User(0,request.form['username'],request.form['password'])
+        loggued_user = ModelUser.login(db,user)
+        if loggued_user != None:
+            if loggued_user.password:
+                login_user(loggued_user)
+                return redirect(url_for('home'))
+            else:
+                flash("Contraseña invalida")
+                return render_template('auth/login.html')
+        else:
+            flash("Usuario no encontrado!")
+            return render_template('auth/login.html')
+    else:
+        return render_template('auth/login.html')
+
+@app.route('/home', methods=['GET'])
+@login_required
+def home():
+    return render_template('home.html')
+
+def status_401(error):
+    return redirect(url_for('login'))
+
+def status_404(error):
+    return "<h1>Pagina no encontrada</h1>",404
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+if __name__ == '__main__':
+    app.config.from_object(config['development'])
+    app.register_error_handler(401,status_401)
+    app.register_error_handler(404,status_404)
+    app.run()
---
+++ FLASK LOGIN/src/config.py
@@ -0,0 +1,18 @@
+from distutils.debug import DEBUG
+
+
+class Config:
+    SECRET_KEY = 'MILLAVESECRETA'
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    
+    
+config = {
+    'development': DevelopmentConfig
+}
+
+direccion_servidor = 'DESKTOP-F9S5AFJ'
+nombre_bd = 'FLASKLOGIN'
+nombre_usuario = 'sa'
+password = '123456'
