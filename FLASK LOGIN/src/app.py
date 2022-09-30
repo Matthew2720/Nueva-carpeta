ed6605ab5074dd7745d4dcb87ed9791d4cf1d26b
Matthew2720 <mhernandezrpo22@gmail.com>
2022-09-30 09:57:23 -0500

    Cambio a home

--- FLASK LOGIN/src/app.py
+++ FLASK LOGIN/src/app.py
@@ -28,7 +28,7 @@ def login():
         if loggued_user != None:
             if loggued_user.password:
                 login_user(loggued_user)
-                return redirect(url_for('casita'))
+                return redirect(url_for('home'))
             else:
                 flash("Contraseña invalida")
                 return render_template('auth/login.html')
